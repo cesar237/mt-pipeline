88edc77b25cf1377c6e9477212451a8c9c00abe4
CesaireMH <nohocesar@gmail.com>
2025-04-20 17:31:08 +0200

    optimize gcc compilation

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Compiling Shared Memory with debugging symbols and no optimization..."
-gcc -g -O0 -lpthread mt-pipeline-shm.c -o mt-pipeline-shm
+gcc -lpthread mt-pipeline-shm.c -o mt-pipeline-shm
 
 echo "Compiling Message Passing with debugging symbols and no optimization..."
-gcc -g -O0 -lpthread mt-pipeline-msp.c -o mt-pipeline-msp
+gcc -lpthread mt-pipeline-msp.c -o mt-pipeline-msp
 
 echo "Compiling Parallel with debugging symbols and no optimization..."
-gcc -g -O0 -lpthread mt-parallel.c -o mt-parallel
+gcc -lpthread mt-parallel.c -o mt-parallel
